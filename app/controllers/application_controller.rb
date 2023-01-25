270020960cc16089fb97cdfbe7a3394cd4fbeddb
hoangbooth <hoang@chicagobooth.edu>
2023-01-25 22:21:39 +0000

    add and substract done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,30 @@
 class ApplicationController < ActionController::Base
-
+###### Addition
   def add_form
 
     render({ :template => "math_templates/add_form.html.erb"})
   end
-
-
   def wizard_add
     @first_num = params.fetch("first_num").to_f
     @second_num = params.fetch("second_num").to_f
     @result = @first_num + @second_num
     render ({ :template => "math_templates/wizard_add.html.erb"})
   end
-
-
-
-
+####### Substraction
+  def subtract_form
+    render ({ :template => "math_templates/subtract_form.html.erb"})
+  end
+  def wizard_subtract
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num - @second_num    
+    render ({ :template => "math_templates/wizard_subtract.html.erb"})
+  end
+####### Multiply form
+  def multiply_form
+    render ({:template => "math_templates/multiply_form.html.erb"})
+    
+  end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get("/add", {:controller =>"application", :action => "add_form"})
   get("/wizard_add", {:controller => "application", :action => "wizard_add"})
 
+  get("/subtract", {:controller =>"application", :action => "subtract_form"})
+  get("/wizard_subtract", {:controller => "application", :action => "wizard_subtract"})
 
+  get("/multiply", {:controller =>"application", :action => "multiply_form"})
+  get("/wizard_multiply", {:controller =>"application", :action => "wizard_multiply"})
 
 end
