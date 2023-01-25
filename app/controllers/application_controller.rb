b2711be08ac8d1a4e2545504028e508d134baa3a
hoangbooth <hoang@chicagobooth.edu>
2023-01-25 21:45:36 +0000

    addition done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
 
+  def add_form
+
+    render({ :template => "math_templates/add_form.html.erb"})
+  end
+
+
+  def wizard_add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
+    render ({ :template => "math_templates/wizard_add.html.erb"})
+  end
+
+
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/", {:controller => "application", :action => "add_form"})
+
+  get("/add", {:controller =>"application", :action => "add_form"})
+  get("/wizard_add", {:controller => "application", :action => "wizard_add"})
+
+
+
 end
